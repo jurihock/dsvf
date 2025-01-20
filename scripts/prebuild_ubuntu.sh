b3d34cd36242ba852cc076c6de1a0833709617a0
Jürgen Hock <juergen.hock@jurihock.de>
2025-01-20 19:58:40 +0100

    Update linux dependencies

--- scripts/prebuild_ubuntu.sh
+++ scripts/prebuild_ubuntu.sh
@@ -3,7 +3,26 @@
 sudo apt update
 
 # build essentials
-sudo apt install --yes build-essential cmake pkg-config
+sudo apt install --yes \
+  build-essential \
+  cmake \
+  pkg-config
 
 # https://github.com/juce-framework/JUCE/blob/master/docs/Linux%20Dependencies.md
-sudo apt install --yes libasound2-dev libjack-jackd2-dev ladspa-sdk libcurl4-openssl-dev libfreetype6-dev libx11-dev libxcomposite-dev libxcursor-dev libxcursor-dev libxext-dev libxinerama-dev libxrandr-dev libxrender-dev libwebkit2gtk-4.0-dev libglu1-mesa-dev mesa-common-dev
+sudo apt install --yes \
+  ladspa-sdk \
+  libasound2-dev \
+  libcurl4-openssl-dev  \
+  libfontconfig1-dev \
+  libfreetype-dev \
+  libglu1-mesa-dev \
+  libjack-jackd2-dev \
+  libwebkit2gtk-4.1-dev \
+  libx11-dev \
+  libxcomposite-dev \
+  libxcursor-dev \
+  libxext-dev \
+  libxinerama-dev \
+  libxrandr-dev \
+  libxrender-dev \
+  mesa-common-dev
