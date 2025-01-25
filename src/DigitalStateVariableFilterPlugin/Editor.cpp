a793228ae86cfc4f2666b4e2d26c95c47f3f9e1c
Jürgen Hock <juergen.hock@jurihock.de>
2025-01-25 18:25:40 +0100

    Make windows build happy

--- src/DigitalStateVariableFilterPlugin/Editor.cpp
+++ src/DigitalStateVariableFilterPlugin/Editor.cpp
@@ -3,7 +3,7 @@
 Editor::Editor(juce::AudioProcessor& process) :
   GenericAudioProcessorEditor(process)
 {
-  const int w = 850 / 1.618, h = 850;
+  const int w = int(850 / 1.618), h = 850;
 
   setSize(w, h);
 }
